11eff5f431f8a6365d23e372408915ab63cc8f54
Abhishek-90 <holaniabhishek90@gmail.com>
2022-01-01 08:44:15 +0530

    Ready for Deploy

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ModeState from "./context/modeState";
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="https://expense-manager-b7b0d.web.app">
           <ModeState>
           <TransactionState>
             <Navbar title="Expense Manager"/>
