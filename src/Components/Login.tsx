85817755589ae34c4b716c0722e12ab990315bca
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-15 19:51:02 +0530

    Changed Login.js to Login.tsx

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,77 +1,92 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router';
-import { authentication } from '../Variables/routes';
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { authentication } from "../Variables/routes";
 
 interface ILoginState {
-    email:string,
-    password:string
+  email: string;
+  password: string;
 }
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [login, setLogin] = useState<ILoginState>({
-        email:"",
-        password:"",
-    })
+  const navigate = useNavigate();
+  const [login, setLogin] = useState<ILoginState>({
+    email: "",
+    password: "",
+  });
 
-    const onChange = (e:any)=>{
-        setLogin({...login, [e.target.name]:e.target.value});
-    }
+  const onChange = (e: any) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
 
-    const onSubmit = async (e:any)=>{
-        e.preventDefault();
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
 
-        try{
-            const response = await fetch(`${authentication}login`,
+    try {
+      const response = await fetch(
+        `${authentication}login`,
 
-                {
-                    method:"POST",
-                    headers:{
-                        'Content-type':'application/json',
-                        'Access-Control-Allow-Origin': '*',
-                    },
-                    body: JSON.stringify({
-                        email: login.email,
-                        password: login.password
-                    })
-                }
-            );
-
-            const json = await response.json();
-
-            if(json.status === 'success'){
-                localStorage.setItem('authToken',json.authToken);
-                console.log(localStorage.getItem('authToken'));
-                navigate('/dashboard');
-            }
-            else{
-                //TODO: Add alert box here to display that Credentials are wrong.
-                
-            }
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            email: login.email,
+            password: login.password,
+          }),
         }
-        catch(error){
-            console.log(error);
-        }
-    }
+      );
 
-    return (
-        <>
-        <div className="container my-4">
-            <h2>Login Page</h2>
-            <form onSubmit={onSubmit}>
-                <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" onChange={onChange}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" onChange={onChange}/>
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
+      const json = await response.json();
+
+      if (json.status === "success") {
+        localStorage.setItem("authToken", json.authToken);
+        console.log(localStorage.getItem("authToken"));
+        navigate("/dashboard");
+      } else {
+        //TODO: Add alert box here to display that Credentials are wrong.
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container my-4">
+      <h2>Login Page</h2>
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+          />
         </div>
-    </>
-    )
-}
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            onChange={onChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
