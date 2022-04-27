f525d20961bbd9ce3a85ea3cb89bf4af3c6a56fa
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-27 12:04:09 +0530

    Changes in workflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TransactionState from "./context/transactionState";
 import ModeState from "./context/modeState";
 // import Dashboard from "./Components/Dashboard/Dashboard";
-// import Login from "./Components/Login/Login";
+import Login from "./Components/Login/Login";
 // import Signup from "./Components/Signup/Signup";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import './index.css';
