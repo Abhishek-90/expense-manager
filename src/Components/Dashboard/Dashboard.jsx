d61e338fedb34f744f2d610fc856b5cc28129183
Abhishek-90 <holaniabhishek90@gmail.com>
2022-07-20 00:03:38 +0530

    Made Dashboard zoom-out responsive

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -3,23 +3,21 @@ import Transaction from "../Transactions/Transaction";
 import Navbar from "../Navbar/Navbar";
 import Menu from "../Menu/Menu";
 // import {AddTransaction} from "../Transactions/AddTransaction";
-import { Container } from "./Dashboard.styled";
+import { MainContainer, Container } from "./Dashboard.styled";
 
 const Dashboard = () => {
   return (
-    <Container>
-      <Navbar />
-      <Menu />
-      <Routes>
-        <Route exact path="/*" element={<Transaction />} />
-        <Route exact path="/loans" element={<Transaction />} />
-        <Route
-          exact
-          path="/investment"
-          element={<Transaction />}
-        />
-      </Routes>
-    </Container>
+    <MainContainer>
+      <Container>
+        <Navbar />
+        <Menu />
+        <Routes>
+          <Route exact path="/*" element={<Transaction />} />
+          <Route exact path="/loans" element={<Transaction />} />
+          <Route exact path="/investment" element={<Transaction />} />
+        </Routes>
+      </Container>
+    </MainContainer>
   );
 };
 
