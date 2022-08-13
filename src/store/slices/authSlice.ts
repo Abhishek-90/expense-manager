62a392ad42c8612a605cffd7a5dc75ea8a9b393e
Abhishek-90 <holaniabhishek90@gmail.com>
2022-08-13 18:40:20 +0530

    Minor changes in Authentication Store

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -2,13 +2,15 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IState { 
   value: {
-    authKey: string
+    authKey: string,
+    isLoggedIn:boolean
   }
 }
 
 const initialState = {
   value: {
     authKey: "",
+    isLoggedIn: false
   },
 };
 
@@ -18,9 +20,16 @@ export const authSlice = createSlice({
   reducers: {
     addAuthToken : (state:IState, action:PayloadAction<string>) => {
       state.value.authKey = action.payload;
+      state.value.isLoggedIn = true;
+    },
+
+    removeAuthToken: (state:IState) => {
+      console.log("Logging Out");
+      state.value.authKey = "";
+      state.value.isLoggedIn = false;
     }
   },
 });
 
-export const { addAuthToken } = authSlice.actions;
+export const { addAuthToken, removeAuthToken } = authSlice.actions;
 export default authSlice.reducer;
