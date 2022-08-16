f87d743bf6765c53428175b70b12150f43f6c078
Abhishek-90 <holaniabhishek90@gmail.com>
2022-08-16 16:13:42 +0530

    Removed Redux store for Auth Token

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,18 +1,7 @@
 import { Container } from "./Dashboard.styled";
 import Navbar from "../Navbar/Navbar";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
-import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  const isLoggedIn:boolean = useSelector((state:RootState) => state.authentication.value.isLoggedIn);
-  const navigate = useNavigate();
-
-  if(!isLoggedIn) {
-    console.log(isLoggedIn);
-    navigate('/');
-  }
-
   return (
     <Container>
       <Navbar />
