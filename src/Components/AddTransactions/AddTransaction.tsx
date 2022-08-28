cdd90b82c1f36c5c32c794e76dd17c3514888d81
Abhishek-90 <holaniabhishek90@gmail.com>
2022-08-28 19:18:33 +0530

    ADD: Add Transaction Button

--- src/Components/AddTransactions/AddTransaction.tsx
+++ src/Components/AddTransactions/AddTransaction.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Container } from './AddTransactions.styled'
+import React from "react";
+import {
+  AddTransactionContainer,
+  Container,
+  SubmitBtn,
+  SubmitBtnWrapper,
+  TransactionDescription,
+  TransactionDescriptionWrapper,
+} from "./AddTransactions.styled";
 
 function AddTransaction() {
   return (
     <Container>
-      
+      <AddTransactionContainer>
+        <TransactionDescriptionWrapper>
+          <TransactionDescription placeholder="Transaction Description" />
+          <SubmitBtnWrapper>
+            <SubmitBtn>Add Transaction</SubmitBtn>
+          </SubmitBtnWrapper>
+        </TransactionDescriptionWrapper>
+      </AddTransactionContainer>
     </Container>
-  )
+  );
 }
 
-export default AddTransaction
+export default AddTransaction;
--- src/Components/AddTransactions/AddTransactions.styled.ts
+++ src/Components/AddTransactions/AddTransactions.styled.ts
@@ -1,7 +1,49 @@
 import styled from "styled-components";
+import * as colors from "../../constants/themeConstants";
+import { Button } from "../Elements/Button";
+
 
 export const Container = styled.div`
   width: 100%;
-  height: 100px;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+`;
+
+export const AddTransactionContainer = styled.div`
+  width: 60%;
+  min-width: 550px;
+  height: 150px;
+  border: 1px black solid;
+  border-radius: 20px;
+  margin-top: 20px;
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+export const TransactionDescriptionWrapper = styled.div`
+  grid-column: 2/ span 2;
+  grid-row: 2/ span 1;
+  padding: 6px;
+`
+export const TransactionDescription = styled.input`
+  height: 100%;
+  width: 100%;
+  padding: 0.6rem;
+  outline:none;
+  font-size: 0.9rem; 
+  border: none;
+  box-shadow: ${colors.shadowBlue} 0px 3px 8px;
+`
+
+export const SubmitBtnWrapper = styled.div`
+  grid-column: 3/span 1;
+  grid-row: 3/span 1;
   background: orange;
+`
+
+export const SubmitBtn = styled(Button)`
+  width: 50%;
+  height: 100%;
 `
