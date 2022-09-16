0918e6ce27132ae9efa415856353b6a116c019ba
Abhishek-90 <holaniabhishek90@gmail.com>
2022-09-16 16:06:36 +0530

    Add Transaction Component UI complete

--- src/Components/AddTransactions/AddTransaction.tsx
+++ src/Components/AddTransactions/AddTransaction.tsx
@@ -8,22 +8,31 @@ import {
   TransactionDescriptionWrapper,
   DateWrapper,
   CustomDate,
+  CustomSelect,
+  SelectWrapper,
 } from "./AddTransactions.styled";
 
 interface ITransactionDetails {
-  date: Date;
+  date: Date|null;
   description: string;
+  type: string;
+  tag: string;
 }
 
 function AddTransaction() {
   const [transactionDetails, setTransactionDetails] =
-    useState<ITransactionDetails>({ date: new Date(), description: "" });
+    useState<ITransactionDetails>({
+      date: null,
+      description: "",
+      type: "income",
+      tag: "",
+    });
 
   function onSubmitClick(): void {
     console.log(transactionDetails);
   }
 
-  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>) {
     setTransactionDetails({
       ...transactionDetails,
       [e.target.name]: e.target.value,
@@ -42,6 +51,30 @@ function AddTransaction() {
             }
           ></CustomDate>
         </DateWrapper>
+        <SelectWrapper>
+          <CustomSelect name="type" placeholder="Type" onChange={(e:React.ChangeEvent<HTMLSelectElement>) => onChangeHandler(e)}>
+            <option value="income">Income</option>
+            <option value="expense">Expense</option>
+          </CustomSelect>
+          {transactionDetails.type === "income" && (
+            <CustomSelect placeholder="Tag">
+              <option value="salary">Salary</option>
+              <option value="interest">Interest</option>
+              <option value="side-income">Side Income</option>
+            </CustomSelect>
+          )}
+          {transactionDetails.type === "expense" && (
+            <CustomSelect placeholder="Tag">
+              <option value="medical">Medical</option>
+              <option value="entertainment">Entertainment</option>
+              <option value="investment">Investment</option>
+              <option value="food">Food</option>
+              <option value="clothing">Clothing</option>
+              <option value="luxury">Luxury</option>
+              <option value="trips">Travel/Trips</option>
+            </CustomSelect>
+          )}
+        </SelectWrapper>
         <TransactionDescriptionWrapper>
           <TransactionDescription
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
