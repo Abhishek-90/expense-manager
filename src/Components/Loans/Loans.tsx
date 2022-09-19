d381f0ea3fc4f943a8731d6c234acf0173d9d64a
Abhishek-90 <holaniabhishek90@gmail.com>
2022-09-19 18:35:29 +0530

    ADD: Static Loans Component

---
+++ src/Components/Loans/Loans.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Loans.styled";
+
+function Loans() {
+  return <Container>Loans Page will be available soon.</Container>;
+}
+
+export default Loans;
