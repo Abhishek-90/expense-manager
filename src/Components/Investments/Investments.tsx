0123dc45e5b43b2997c2cb3a1e6ea7592cadee83
Abhishek-90 <holaniabhishek90@gmail.com>
2022-09-19 18:36:41 +0530

    ADD: Investment Component

---
+++ src/Components/Investments/Investments.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Investments.styled";
+
+function Loans() {
+  return <Container>Investments Page will be available soon.</Container>;
+}
+
+export default Loans;
