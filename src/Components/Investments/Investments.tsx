c63495c0021fa7c34365999bf602c1c39233c646
Abhishek-90 <holaniabhishek90@gmail.com>
2022-09-19 18:38:39 +0530

    ADD: Visuals Component

--- src/Components/Investments/Investments.tsx
+++ src/Components/Investments/Investments.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container } from "./Investments.styled";
 
-function Loans() {
+function Investments() {
   return <Container>Investments Page will be available soon.</Container>;
 }
 
-export default Loans;
+export default Investments;
---
+++ src/Components/Visuals/Visuals.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Visuals.styled";
+
+function Visuals() {
+  return <Container>Visuals Page will be available soon.</Container>;
+}
+
+export default Visuals;
