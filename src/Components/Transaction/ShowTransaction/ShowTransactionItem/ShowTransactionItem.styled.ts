0f805dfd69c7c4a0bc14adfcf7670155ed56ac0f
Abhishek-90 <holaniabhishek90@gmail.com>
2022-09-29 17:00:06 +0530

    Created ShowTransactionItem

---
+++ src/Components/Transaction/ShowTransaction/ShowTransactionItem/ShowTransactionItem.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const DateWrapper = styled.div`
+  flex: 0.4;
+  background: orange;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
---
+++ src/Components/Transaction/ShowTransaction/ShowTransactionItem/ShowTransactionItem.tsx
@@ -0,0 +1,9 @@
+import { ItemContainer, DateWrapper } from "./ShowTransactionItem.styled";
+
+function ShowTransactionItem() {
+  return <ItemContainer>
+    <DateWrapper></DateWrapper>
+  </ItemContainer>;
+}
+
+export default ShowTransactionItem;
