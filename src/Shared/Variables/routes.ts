153b4d2257e7ee4d5abfed27cf5059107038d94c
Abhishek-90 <holaniabhishek90@gmail.com>
2022-11-02 14:35:21 +0530

    added dynamic env routes

--- src/Shared/Variables/routes.ts
+++ src/Shared/Variables/routes.ts
@@ -1,5 +1,5 @@
-export const authentication:string = "http://localhost:5000/auth/";
-export const ADDTRANSACTION:string = "http://localhost:5000/transaction/addtransaction";
-export const LOGIN:string = "http://localhost:5000/auth/login";
-export const LOGOUT:string = "http://localhost:5000/auth/logout";
-export const AUTOLOGIN:string = "http://localhost:5000/auth/autoLogin";
+export const SINGUP:string = `${process.env.REACT_APP_BACKEND_URL}auth/signup`;
+export const ADDTRANSACTION:string = `${process.env.REACT_APP_BACKEND_URL}transaction/addtransaction`;
+export const LOGIN:string = `${process.env.REACT_APP_BACKEND_URL}auth/login`;
+export const LOGOUT:string = `${process.env.REACT_APP_BACKEND_URL}auth/logout`;
+export const AUTOLOGIN:string = `${process.env.REACT_APP_BACKEND_URL}auth/autoLogin`;
