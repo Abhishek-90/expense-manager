393e02cda3e95f85526428ad5cab57c0a9d44b39
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-12 11:35:58 +0530

    clean up

--- src/Components/Transaction/ShowTransaction/ShowTransaction.tsx
+++ src/Components/Transaction/ShowTransaction/ShowTransaction.tsx
@@ -1,73 +1,67 @@
-import {
-  Container,
-  TableHeader,
-  DateColumnWrapper,
-  AmountColumnWrapper,
-  DescriptionColumnWrapper,
-} from "./ShowTransaction.styled";
+import * as S from "./ShowTransaction.styled";
 import ShowTransactionItem from "./ShowTransactionItem/ShowTransactionItem";
 
 const ShowTransaction = () => {
   const transactions = [
-        {
-          "_id": "63418dc8e52687a600c66192",
-          "email": "atons@gmail.com",
-          "date": "2022-10-08",
-          "type": "expense",
-          "amount": "500",
-          "description": "Chinchoti Waterfall",
-          "tag": "trips",
-          "__v": 0
-        },
-        {
-          "_id": "6341910d6f3335c4098ed77f",
-          "email": "atons@gmail.com",
-          "date": "2022-10-08",
-          "type": "expense",
-          "amount": "500",
-          "description": "Chinchoti Waterfall",
-          "tag": "trips",
-          "__v": 0
-        },
-        {
-          "_id": "6344016c03e78c16a4221732",
-          "email": "atons@gmail.com",
-          "date": "2022-10-11",
-          "type": "expense",
-          "amount": "2000",
-          "description": "Temp2",
-          "tag": "salary",
-          "__v": 0
-        },
-        {
-          "_id": "6344035003e78c16a4221734",
-          "email": "atons@gmail.com",
-          "date": "2022-10-11",
-          "type": "expense",
-          "amount": "2000",
-          "description": "Temp2",
-          "tag": "salary",
-          "__v": 0
-        },
-        {
-          "_id": "6344124a03e78c16a4221747",
-          "email": "atons@gmail.com",
-          "date": "2022-10-11",
-          "type": "expense",
-          "amount": "2000",
-          "description": "Temp Temp",
-          "tag": "salary",
-          "__v": 0
-        }
+    {
+      _id: "63418dc8e52687a600c66192",
+      email: "atons@gmail.com",
+      date: "2022-10-08",
+      type: "expense",
+      amount: "500",
+      description: "Chinchoti Waterfall",
+      tag: "trips",
+      __v: 0,
+    },
+    {
+      _id: "6341910d6f3335c4098ed77f",
+      email: "atons@gmail.com",
+      date: "2022-10-08",
+      type: "expense",
+      amount: "500",
+      description: "Chinchoti Waterfall",
+      tag: "trips",
+      __v: 0,
+    },
+    {
+      _id: "6344016c03e78c16a4221732",
+      email: "atons@gmail.com",
+      date: "2022-10-11",
+      type: "expense",
+      amount: "2000",
+      description: "Temp2",
+      tag: "salary",
+      __v: 0,
+    },
+    {
+      _id: "6344035003e78c16a4221734",
+      email: "atons@gmail.com",
+      date: "2022-10-11",
+      type: "expense",
+      amount: "2000",
+      description: "Temp2",
+      tag: "salary",
+      __v: 0,
+    },
+    {
+      _id: "6344124a03e78c16a4221747",
+      email: "atons@gmail.com",
+      date: "2022-10-11",
+      type: "expense",
+      amount: "2000",
+      description: "Temp Temp",
+      tag: "salary",
+      __v: 0,
+    },
   ];
 
   return (
-    <Container>
-      <TableHeader>
-        <DateColumnWrapper>Date</DateColumnWrapper>
-        <AmountColumnWrapper>Amount</AmountColumnWrapper>
-        <DescriptionColumnWrapper>Description</DescriptionColumnWrapper>
-      </TableHeader>
+    <S.Container>
+      <S.TableHeader>
+        <S.DateColumnWrapper>Date</S.DateColumnWrapper>
+        <S.AmountColumnWrapper>Amount</S.AmountColumnWrapper>
+        <S.DescriptionColumnWrapper>Description</S.DescriptionColumnWrapper>
+      </S.TableHeader>
       {transactions.map((item) => {
         return (
           <ShowTransactionItem
@@ -77,7 +71,7 @@ const ShowTransaction = () => {
           />
         );
       })}
-    </Container>
+    </S.Container>
   );
 };
 
