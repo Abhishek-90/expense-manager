a03dfe11455ef583eb6bcde58833a92e6984666c
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-12 12:28:09 +0530

    minor change

--- src/Shared/Elements/Spinner.tsx
+++ src/Shared/Elements/Spinner.tsx
@@ -1,20 +1,18 @@
 import styled from "styled-components";
 
 const SpinnerContainer = styled.div`
-  height: 100%;
-  margin: 0;
-  padding:0;
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 100%;
 `;
 
 const Spinner = () => {
   return (
     <SpinnerContainer>
-      <img src={require('../Media/Spinner.gif')} alt='Loading ...'/>
+      <img src={require("../Media/Spinner.gif")} alt="Loading ..." />
     </SpinnerContainer>
-  )
-}
+  );
+};
 
 export default Spinner;
