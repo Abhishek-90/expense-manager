48d3824e4e069249254c74832b46433550724939
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-12 20:35:01 +0530

    spinner and route

--- src/Shared/Variables/routes.ts
+++ src/Shared/Variables/routes.ts
@@ -4,3 +4,4 @@ export const LOGIN: string = `${process.env.REACT_APP_BACKEND_URL}auth/login`;
 export const LOGOUT: string = `${process.env.REACT_APP_BACKEND_URL}auth/logout`;
 export const AUTOLOGIN: string = `${process.env.REACT_APP_BACKEND_URL}auth/autoLogin`;
 export const GETTRANSACTION: string = `${process.env.REACT_APP_BACKEND_URL}transaction/statement`;
+export const DELETETRANSACTION: string = `${process.env.REACT_APP_BACKEND_URL}transaction/remove`;
