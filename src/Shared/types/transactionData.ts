552e29776fe7a1516f6f622a1d338ae24080436c
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-12 16:38:35 +0530

    transaction data type added

---
+++ src/Shared/types/transactionData.ts
@@ -0,0 +1,10 @@
+export interface ITransactionState {
+  _id: string;
+  email: string;
+  date: string;
+  type: string;
+  amount: string;
+  description: string;
+  tag: string;
+  __v: number;
+}
