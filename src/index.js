77bb735e51635f81b20c166599dc5777d365c758
Abhishek-90 <holaniabhishek90@gmail.com>
2023-03-12 17:10:51 +0530

    minor updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./globalStyles.css";
 import { Provider } from "react-redux";
@@ -19,5 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals();
